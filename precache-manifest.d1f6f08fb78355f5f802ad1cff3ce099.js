4d92532f3e1b4334f70bf51c093e0979d3308109
Hurrell <cem.hurrell@gmail.com>
2020-04-25 18:03:46 +0100

    Updates

---
+++ precache-manifest.d1f6f08fb78355f5f802ad1cff3ce099.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac57fb4bbb96862f071871f9e7bd1fb",
+    "url": "/patchpalapp/index.html"
+  },
+  {
+    "revision": "4f6e2e9367485ca380c5",
+    "url": "/patchpalapp/static/css/main.1ef87ec3.chunk.css"
+  },
+  {
+    "revision": "855c359e44f2e6f93819",
+    "url": "/patchpalapp/static/js/2.74ff3adb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/patchpalapp/static/js/2.74ff3adb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f6e2e9367485ca380c5",
+    "url": "/patchpalapp/static/js/main.3605d678.chunk.js"
+  },
+  {
+    "revision": "0be4d093d326b0343fc4",
+    "url": "/patchpalapp/static/js/runtime-main.ba88b367.js"
+  }
+]);
