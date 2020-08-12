87c1736756742c35e82f3e546ef3a1573378a22d
Hurrell <cem.hurrell@gmail.com>
2020-08-12 15:28:40 +0100

    Updates

---
+++ precache-manifest.772926d6a84892b2507ca722fbea465f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b49196eef711b944429e80df09ddb9fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b63ffb93ed9607eac4dc",
+    "url": "/static/css/main.bb23f854.chunk.css"
+  },
+  {
+    "revision": "a0e21ea3f98e330e1830",
+    "url": "/static/js/2.bff70c87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bff70c87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b63ffb93ed9607eac4dc",
+    "url": "/static/js/main.52bdb2d5.chunk.js"
+  },
+  {
+    "revision": "106a9ecb1b12f6cec235",
+    "url": "/static/js/runtime-main.db189513.js"
+  },
+  {
+    "revision": "bd9aa86c7ff939a1bddc6729d94195a0",
+    "url": "/static/media/dmx3_female.bd9aa86c.svg"
+  },
+  {
+    "revision": "10c7fcd94985e21e6d090dfa93f79ba0",
+    "url": "/static/media/dmx3_male.10c7fcd9.svg"
+  },
+  {
+    "revision": "f27a99613a1a6974747d7ecc646e1216",
+    "url": "/static/media/dmx5_female.f27a9961.svg"
+  },
+  {
+    "revision": "209f6d47511cc10b0c5c76825da7b607",
+    "url": "/static/media/dmx5_male.209f6d47.svg"
+  },
+  {
+    "revision": "286871b3175b51eccbfe718b5eba5989",
+    "url": "/static/media/ethercon.286871b3.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.660d5dc079e2b405d39250d4085d89d1.js"
+  "/precache-manifest.772926d6a84892b2507ca722fbea465f.js"
 );
 
 self.addEventListener('message', (event) => {
