ccb827567ef21f13d1da31ee2981d70815637438
Hurrell <cem.hurrell@gmail.com>
2022-04-25 22:28:22 +0100

    testing circleci

---
+++ src/tests/group2.test.js
@@ -0,0 +1,11 @@
+const { wait } = require("@testing-library/react");
+
+describe("this is the first set of tests", () => {
+  it("runs a test", async () => {
+    console.log(1);
+    await new Promise((resolve) => setTimeout(resolve, 3000)); // 3 sec
+    console.log(2);
+
+    expect(true);
+  });
+});
